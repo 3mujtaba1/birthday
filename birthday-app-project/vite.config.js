53ce1ed2e517b0c758ba0a6bb65b31d5c7de26fe
“Mujtaba <msjtaba31@gmail.com>
2025-10-02 10:02:05 +0300

    Last Commit

--- birthday-app-project/vite.config.js
+++ birthday-app-project/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/birthday-website-for-special-one'
+  base: '/special-birthday-website/'
 })
